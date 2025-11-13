6cb3f74bdde752d1e953011bb63c5637722121ad
Abdulloh <abdullo2008a@gmail.com>
2025-11-13 21:53:33 +0500

    fix: add 'core' to INSTALLED_APPS to resolve Session model error

--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "core",
+    "django-jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
